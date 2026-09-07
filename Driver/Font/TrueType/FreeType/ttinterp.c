7c152c607032498e579a0bf815fdc30cb62e48a8
Jirka Kunze <jirka.kunze@googlemail.com>
2026-09-07 20:05:09 +0200

    Fix F_dot_P calculation in bytecode interpreter.

--- Driver/Font/TrueType/FreeType/ttinterp.c
+++ Driver/Font/TrueType/FreeType/ttinterp.c
@@ -1424,8 +1424,8 @@
       }
       else
       {
-        CUR.F_dot_P = (Long)CUR.GS.projVector.x * CUR.GS.freeVector.x << 2 +
-                      (Long)CUR.GS.projVector.y * CUR.GS.freeVector.y << 2;
+        CUR.F_dot_P = (Long)CUR.GS.projVector.x * CUR.GS.freeVector.x * 4 +
+                      (Long)CUR.GS.projVector.y * CUR.GS.freeVector.y * 4;
       }
     }
 
